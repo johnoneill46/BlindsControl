aea9ef78a272f289fe09f62182c77606e0307cfe
johnoneill46 <johnoneill546@gmail.com>
2017-04-03 00:35:40 +0100

    Minor fixes & code cleanup

--- BlindsControl/BlindsControl.ino
+++ BlindsControl/BlindsControl.ino
@@ -4,70 +4,65 @@
  #include <Servo.h>
   
     const int buttonPin = 2;
-    const int buttonPin2 = 4;
     const int servoPin = 9;
-    int rightButton;
-    Servo servoA;
+    Servo servo;
     int pos = 0;
-    bool buttonBool = false;
-    bool active = false;   // to be used to know if should be open or closed
+    int buttonState = 0;         // current state of the button
+    int lastButtonState = 0;     // previous state of the button
   
   void setup() 
   {
-    servoA.attach(servoPin);
+    
+    servo.attach(servoPin);
     Serial.begin(9600);  
     pinMode(buttonPin, INPUT);
+    servo.write(pos);
     Serial.println("Application Starting...");
+
   }
   
   void loop()
 { 
-     
-    if (digitalRead(buttonPin) == LOW){     // Button NOT pressed
+     // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
 
-       closeBlinds();
-    }
-     
-     if (digitalRead(buttonPin) == HIGH) {     //Button Pressed
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
 
+      if (buttonState == HIGH) {    // if current state is HIGH then button went from off to on:
+        Serial.println("On");
         openBlinds();
-      
-     }
-
-
-
-
-
-
-  }
-
-
-  void checkForButtonChange(){
-      if(buttonBool = true){
-        //button pressed to open
-        openBlinds();
-      }
-      else{
-        //button pressed to close
+      } else {                      // if current state is LOW then the button went from on to off:
+        Serial.println("Off");
         closeBlinds();
       }
+      // Delay a little bit to avoid bouncing
+      delay(50);
   }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+
+   
+
+  }
+
 
   void openBlinds(){
 
-       for(pos = 180; pos>=180; pos -=180)     // go from 90 degrees to 0 degrees 
+       for(pos = 180; pos>=180; pos -=180)     // go from 180 degrees to 0 degrees 
        {                                
-          servoA.write(pos);
+          servo.write(pos);
           Serial.println("Button Pressed - Opening Blinds");
-       delay(1000);
+          delay(2000);
        } 
   }
 
   void closeBlinds(){
 
-      for(pos = 0; pos<90; pos += 90)  // go from 0 degrees to 90 degrees 
+      for(pos = 0; pos<180; pos += 180)  // go from 0 degrees to 90 degrees 
          {
-           servoA.write(pos);
+           servo.write(pos);
          } 
   }
 
