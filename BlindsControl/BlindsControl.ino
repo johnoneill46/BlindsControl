7b6b1a847706fb61ebf3eb43c222c99ad6c8e1c8
johnoneill46 <johnoneill546@gmail.com>
2017-04-03 00:03:32 +0100

    Pressing button moves servo before returning to original position - only 1 button now

--- BlindsControl/BlindsControl.ino
+++ BlindsControl/BlindsControl.ino
@@ -5,46 +5,69 @@
   
     const int buttonPin = 2;
     const int buttonPin2 = 4;
-    int buttonState = 0;
-    int buttonState2 = 0;
+    const int servoPin = 9;
+    int rightButton;
     Servo servoA;
-    int position = 0;
+    int pos = 0;
+    bool buttonBool = false;
+    bool active = false;   // to be used to know if should be open or closed
   
   void setup() 
   {
-    servoA.attach(9);
+    servoA.attach(servoPin);
     Serial.begin(9600);  
     pinMode(buttonPin, INPUT);
-    pinMode(buttonPin2,INPUT);
-    //servoA.write(90);
     Serial.println("Application Starting...");
   }
   
-  void loop() 
-  {
-  
-      buttonState = digitalRead(buttonPin);   // right button 
-      buttonState2 = digitalRead(buttonPin2); 
-      
-     if(buttonState2 == HIGH){
-      //  servoA.write(position++);
-      
-   Serial.println("button state 1 HIGH");
-       
-     //   servoA.write(0); 
-     //   delay(5); 
-        
-  }
-  
-    if(buttonState2 == LOW){
-     //   servoA.write(position--);
-
+  void loop()
+{ 
      
-    Serial.println("button state 1 LOW");
-      
-     //  servoA.write(170); 
-    
+    if (digitalRead(buttonPin) == LOW){     // Button NOT pressed
+
+       closeBlinds();
     }
-  
+     
+     if (digitalRead(buttonPin) == HIGH) {     //Button Pressed
+
+        openBlinds();
+      
+     }
+
+
+
+
+
+
+  }
+
+
+  void checkForButtonChange(){
+      if(buttonBool = true){
+        //button pressed to open
+        openBlinds();
+      }
+      else{
+        //button pressed to close
+        closeBlinds();
+      }
+  }
+
+  void openBlinds(){
+
+       for(pos = 180; pos>=180; pos -=180)     // go from 90 degrees to 0 degrees 
+       {                                
+          servoA.write(pos);
+          Serial.println("Button Pressed - Opening Blinds");
+       delay(1000);
+       } 
+  }
+
+  void closeBlinds(){
+
+      for(pos = 0; pos<90; pos += 90)  // go from 0 degrees to 90 degrees 
+         {
+           servoA.write(pos);
+         } 
   }
 
