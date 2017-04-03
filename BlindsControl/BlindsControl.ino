79018a6eed42df75261e956a37ede2d3841a67ae
johnoneill46 <johnoneill546@gmail.com>
2017-04-03 01:32:14 +0100

    Code cleanup and minor improvements

--- BlindsControl/BlindsControl.ino
+++ BlindsControl/BlindsControl.ino
@@ -6,68 +6,57 @@
     const int buttonPin = 2;
     const int servoPin = 9;
     Servo servo;
+    
     int pos = 0;
     int buttonState = 0;         // current state of the button
     int lastButtonState = 0;     // previous state of the button
-
-    int x = 0;
     int oldButtonState = LOW;
+    bool blindsOpen = false;
   
-  void setup() 
-  {
+  void setup(){
     
     servo.attach(servoPin);
     Serial.begin(9600);  
     pinMode(buttonPin, INPUT);
     servo.write(pos);
-    Serial.println("Application Starting...");
-
+    Serial.println("Application Started.");
   }
   
-  void loop()
-{ 
-          // Get the current state of the button
-      int newButtonState = digitalRead(buttonPin);
-    
-          // checks if button has changed since we last read it
-      if (newButtonState == HIGH && oldButtonState == LOW) {
-    
-        if (x == 0) {
-          // Toggle on
-          openBlinds();
-          x = 1;
-    
-        } else {
-          // Toggle off
-          closeBlinds();
-          x = 0;
+   void loop(){ 
+            
+           // Get the current state of the button
+        int newButtonState = digitalRead(buttonPin);
+      
+           // checks if button has changed since we last read it
+        if (newButtonState == HIGH && oldButtonState == LOW) {
+          
+              if (!blindsOpen){
+                  openBlinds();
+                  blindsOpen = true;
+          
+               } else{
+                  closeBlinds();
+                  blindsOpen = false;
+              }
+                 delay(50);
         }
-        delay(50);
-      }
-    
-      // Store the button's state so we can tell if it's changed next time round
-      oldButtonState = newButtonState;
-
-  }
+      
+        // Store the button's state so we can tell if it's changed next time round
+        oldButtonState = newButtonState;
+   }
 
 
   void openBlinds(){
-
-       for(pos = 180; pos>=180; pos -=180)     // go from 180 degrees to 0 degrees 
-       {                                
-          servo.write(pos);
-          Serial.println("Button Pressed - Opening Blinds");
+                               
+          servo.write(90);
+          Serial.println("Opening Blinds");
           delay(20);
-       } 
   }
 
   void closeBlinds(){
-
-      for(pos = 0; pos<180; pos += 180)  // go from 0 degrees to 90 degrees 
-         {
-           servo.write(pos);
-           Serial.println("Button Pressed - Closing Blinds");
+    
+           servo.write(0);  // set to 5 to accomodate for slight offset with servo
+           Serial.println("Closing Blinds");
            delay(20);
-         } 
   }
 
