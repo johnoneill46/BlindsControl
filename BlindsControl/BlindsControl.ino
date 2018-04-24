50664961b8b1dbeaa550138b3986d50c6bb88011
John O'Neill <johnon@kainos.com>
2018-04-25 00:56:03 +0100

    Updated ReadMe.md and code cleanup

--- BlindsControl/BlindsControl.ino
+++ BlindsControl/BlindsControl.ino
@@ -1,29 +1,15 @@
-/*
- * Arduino program that will move servo depending on button pressed
-
-
- TODO:
- - extend servo arms to increase pullable distance
- - Connect to internet
- - Call servo change via internet call?
- - Alexa app to control
+#include <Servo.h>
  
- */
- 
- #include <Servo.h>
+Servo servo;
+const int buttonPin = 2;
+const int servoPin = 9;
+bool blindsOpen = false;
+int pos = 0;
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;    // previous state of the button
+int oldButtonState = LOW;
   
-    const int buttonPin = 2;
-    const int servoPin = 9;
-    Servo servo;
-    
-    int pos = 0;
-    int buttonState = 0;         // current state of the button
-    int lastButtonState = 0;     // previous state of the button
-    int oldButtonState = LOW;
-    bool blindsOpen = false;
-  
-  void setup(){
-    
+void setup(){
     servo.attach(servoPin);
     Serial.begin(9600);  
     pinMode(buttonPin, INPUT);
@@ -31,41 +17,30 @@
     Serial.println("Application Started.");
   }
   
-   void loop(){ 
-            
-           // Get the current state of the button
-        int newButtonState = digitalRead(buttonPin);
-      
-           // checks if button has changed since we last read it
-        if (newButtonState == HIGH && oldButtonState == LOW) {
-          
-              if (!blindsOpen){
-                  openBlinds();
-                  blindsOpen = true;
-          
-               } else{
-                  closeBlinds();
-                  blindsOpen = false;
-              }
-                 delay(50);
-        }
-      
-        // Store the button's state so we can tell if it's changed next time round
-        oldButtonState = newButtonState;
-   }
-
-
-  void openBlinds(){
-                               
-          servo.write(90);
-          Serial.println("Opening Blinds");
-          delay(20);
-  }
-
-  void closeBlinds(){
+void loop(){ 
+    int newButtonState = digitalRead(buttonPin);    // Get current button state
     
-           servo.write(0);  // set to 5 to accomodate for slight offset with servo
-           Serial.println("Closing Blinds");
-           delay(20);
-  }
+    if (newButtonState == HIGH && oldButtonState == LOW) {        // checks if button has changed since last read
+         if (blindsOpen){
+              closeBlinds();
+              blindsOpen = false;       
+         }else{
+              openBlinds();
+              blindsOpen = true;
+         }
+      delay(50);
+    }
+    oldButtonState = newButtonState;    // Store the button's state so we can tell if it's changed next time round
+ }
 
+void openBlinds(){
+     servo.write(90);
+     Serial.println("Opening Blinds")
+     delay(20);
+}
+
+void closeBlinds(){
+     servo.write(0);    // set to 5 to accomodate for slight offset with servo
+     Serial.println("Closing Blinds");
+     delay(20);
+}
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # Blinds Control
 
-### Based on the following http://lifehacker.com/control-arduino-powered-blinds-with-an-alexa-voice-comm-1771554473
-### Will create push button operated servo that can be used to pull each toggle for the blinds
-### Alexa voice control integration will then be implemented
-
-#### TODO:
-#### - Connect to internet
-#### - extend servo arm length to pull cord properly  
-#### - Alexa App
+### Blind control application using Arduino based on: http://lifehacker.com/control-arduino-powered-blinds-with-an-alexa-voice-comm-1771554473
